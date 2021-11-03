3f735dc3fae94433b06d28e9d8aa1d6aef4f6c3e
nickaxgit <nickax@gmail.com>
2021-11-03 14:56:48 +0000

    added fetch

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-alert ("hi there")
+async function fetchData(url) {  //Uniform Resource Locator
+    let headers = await fetch(url);   //Get the headers 
+    let data = await headers.text();  //get the document 'payload'
+    
+    let obj=JSON.parse(data)
+
+    alert(obj.setup) 
+
+    /*if (joke.flags.nsfw || joke.flags.racist || joke.flags.sexist   ){
+        let tooRude=document.createElement("H2")    
+        document.body.appendChild(tooRude)
+        tooRude.innerHTML="Too Rude to tell"
+    }
+    else{
+
+        let setup=document.createElement("H2")
+        document.body.appendChild(setup)
+        setup.innerHTML=joke.setup
+
+        let delivery = document.createElement("H2")
+        document.body.appendChild(delivery)
+        delivery.innerHTML=joke.delivery
+    }
+    */
+   
+  }
+
+  
+  fetchData("https://v2.jokeapi.dev/joke/any") //Hit the endpoint of the service
+  
+
+
+
+ 
+

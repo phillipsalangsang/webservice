627c31c91a47f71a6a2dd3654bd92ab1e54c089d
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-11-04 09:48:38 +0000

    dogs

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
+"use strict"
+
+let img = document.createElement("img")
+document.body.appendChild(img)
+
 async function fetchData(url) {  //Uniform Resource Locator
     let headers = await fetch(url);   //Get the headers 
     let data = await headers.text();  //get the document 'payload'
-    
-    let obj=JSON.parse(data)
 
-    alert(obj.setup) 
-
-    /*if (joke.flags.nsfw || joke.flags.racist || joke.flags.sexist   ){
-        let tooRude=document.createElement("H2")    
-        document.body.appendChild(tooRude)
-        tooRude.innerHTML="Too Rude to tell"
-    }
-    else{
-
-        let setup=document.createElement("H2")
-        document.body.appendChild(setup)
-        setup.innerHTML=joke.setup
-
-        let delivery = document.createElement("H2")
-        document.body.appendChild(delivery)
-        delivery.innerHTML=joke.delivery
-    }
-    */
-   
+    let randomDog = JSON.parse(data)
+    img.src = randomDog.url
   }
 
-  
-  fetchData("https://open.er-api.com/v6/latest/USD") //Hit the endpoint of the service
-  
+
+function dogPics(){
+fetchData("https://random.dog/woof.json") //Hit the endpoint of the service
+}
+
 
 
 
